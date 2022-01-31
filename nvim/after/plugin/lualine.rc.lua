715d7ebb979476b51a7237aeba82f2ce4ee3c233
mpanasiewicz <maciej.panasiewicz@simplybusiness.co.uk>
2022-01-31 15:14:50 +0100

    LCP config modifications
    - Add LSP information into lualine
    - Add better encoding checking
    - Add full file path name

--- nvim/after/plugin/lualine.rc.lua
+++ nvim/after/plugin/lualine.rc.lua
@@ -1,6 +1,37 @@
 local status, lualine = pcall(require, "lualine")
 if (not status) then return end
 
+local conditions = {
+  buffer_not_empty = function()
+    return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
+  end,
+  hide_in_width = function()
+    return vim.fn.winwidth(0) > 80
+  end,
+  check_git_workspace = function()
+    local filepath = vim.fn.expand('%:p:h')
+    local gitdir = vim.fn.finddir('.git', filepath .. ';')
+    return gitdir and #gitdir > 0 and #gitdir < #filepath
+  end,
+}
+
+-- ######## NOT USED #######
+local colors = {
+  bg       = '#202328',
+  fg       = '#bbc2cf',
+  yellow   = '#ECBE7B',
+  cyan     = '#008080',
+  darkblue = '#081633',
+  green    = '#98be65',
+  orange   = '#FF8800',
+  violet   = '#a9a1e1',
+  magenta  = '#c678dd',
+  blue     = '#51afef',
+  red      = '#ec5f67',
+  white    = '#ffffff',
+}
+-- #########################
+
 lualine.setup {
   options = {
     icons_enabled = true,
@@ -12,15 +43,42 @@ lualine.setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch'},
-    lualine_c = {{
-      'filename',
-      file_status = true, -- displays file status (readonly status, modified status)
-      path = 0 -- 0 = just filename, 1 = relative path, 2 = absolute path
-    }},
+    lualine_c = {
+      {
+        'filename',
+        file_status = true, -- displays file status (readonly status, modified status)
+        path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+      }, {
+           function()
+             return '%='
+           end,
+      }, {
+           -- Lsp server name .
+           function()
+             local msg = 'No Active Lsp'
+             local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+             local clients = vim.lsp.get_active_clients()
+             if next(clients) == nil then
+               return msg
+             end
+             for _, client in ipairs(clients) do
+               local filetypes = client.config.filetypes
+               if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                 return client.name
+               end
+             end
+             return msg
+           end,
+           icon = ' LSP:'
+      }
+    },
     lualine_x = {
       { 'diagnostics', sources = {"nvim_diagnostic"}, symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '} },
-      'encoding',
+      { 'o:encoding', -- option component same as &encoding in viml
+        fmt = string.upper, -- I'm not sure why it's upper case either ;)
+        cond = conditions.hide_in_width },
       'filetype'
+
     },
     lualine_y = {'progress'},
     lualine_z = {'location'}
