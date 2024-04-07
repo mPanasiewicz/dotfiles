35725d89ad235657b1cf41c57abb4784079bef8d
mpanasiewicz <maciej.panasiewicz@simplybusiness.co.uk>
2024-04-07 17:58:34 +0200

    feat(copilot): Initialization

--- nvim/lua/maciejpanasiewicz/plugins.lua
+++ nvim/lua/maciejpanasiewicz/plugins.lua
@@ -32,6 +32,7 @@ packer.startup(function(use)
     'nvim-treesitter/nvim-treesitter',
     run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
   }
+  use 'github/copilot.vim'
   use 'kyazdani42/nvim-web-devicons' -- File icons
   use 'nvim-telescope/telescope.nvim'
   use 'nvim-telescope/telescope-file-browser.nvim'
