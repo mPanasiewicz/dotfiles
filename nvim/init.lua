f32de21da3e89030d26060621ada56d2242c8e08
mpanasiewicz <maciej.panasiewicz@simplybusiness.co.uk>
2024-04-07 17:37:08 +0200

    feat(nvim): add linux config

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,10 +7,14 @@ require('maciejpanasiewicz.plugins')
 local has = vim.fn.has
 local is_mac = has "macunix"
 local is_win = has "win32"
+local is_linux = has "unix"
 
 if is_mac then
   require('maciejpanasiewicz.macos')
 end
 if is_win then
   require('maciejpanasiewicz.windows')
+end
+if is_linux == 1 then
+  require('maciejpanasiewicz.linux')
 end
