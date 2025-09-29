ce77feba81e986fa321a81ece62d420b2a4926cc
mpanasiewicz <maciej.panasiewicz@simplybusiness.co.uk>
2025-09-29 16:37:43 +0100

    remove: copilot from carftzdog config

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -45,28 +45,6 @@ return {
     end,
   },
 
-  -- copilot
-  {
-    "zbirenbaum/copilot.lua",
-    opts = {
-      suggestion = {
-        auto_trigger = true,
-        keymap = {
-          accept = "<C-l>",
-          accept_word = "<M-l>",
-          accept_line = "<M-S-l>",
-          next = "<M-]>",
-          prev = "<M-[>",
-          dismiss = "<C-]>",
-        },
-      },
-      filetypes = {
-        markdown = true,
-        help = true,
-      },
-    },
-  },
-
   -- copilot chat with Claude Sonnet model
   {
     "CopilotC-Nvim/CopilotChat.nvim",
