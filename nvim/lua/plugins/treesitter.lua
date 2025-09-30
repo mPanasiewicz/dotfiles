eb9e93637a48ce671a417f60df07eeb6fe2f4825
mpanasiewicz <maciej.panasiewicz@simplybusiness.co.uk>
2025-09-30 07:51:25 +0100

    refactor: remove playground plugin

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
-  { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
+  -- Remove deprecated playground plugin
+  -- { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
 
   {
     "nvim-treesitter/nvim-treesitter",
@@ -69,36 +70,9 @@ return {
         use_virtual_text = true,
         lint_events = { "BufWrite", "CursorHold" },
       },
-
-      playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = true, -- Whether the query persists across vim sessions
-        keybindings = {
-          toggle_query_editor = "o",
-          toggle_hl_groups = "i",
-          toggle_injected_languages = "t",
-          toggle_anonymous_nodes = "a",
-          toggle_language_display = "I",
-          focus_language = "f",
-          unfocus_language = "F",
-          update = "R",
-          goto_node = "<cr>",
-          show_help = "?",
-        },
-      },
     },
     config = function(_, opts)
       require("nvim-treesitter").setup(opts)
-      
-      -- Force ensure Ruby parser is properly installed
-      vim.defer_fn(function()
-        local has_parser = pcall(require, "nvim-treesitter.parsers")
-        if has_parser then
-          vim.cmd("TSInstall! ruby")
-        end
-      end, 100)
 
       -- MDX
       vim.filetype.add({
